75d612d36a4770fc790c665ae92f87b6244e2a5f
Jelin <6531501042@lamduan.mfu.ac.th>
2025-09-20 11:18:08 +0700

    เพิ่ม navbar

--- FE/src/router/secretary.js
+++ FE/src/router/secretary.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../Login.vue'
+// Layouts
 import Appointment from '@/views/secretary/Appointment/Appointment.vue'
 
 export default [
-  { path: '/dashbord', name: 'DashboardSE', component: Appointment },
+  { path: '/Appointment', name: 'Appointment', component: Appointment }, 
  
 ]
 
