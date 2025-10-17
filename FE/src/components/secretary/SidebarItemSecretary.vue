af8708e3c3dbd5afcfccddcf8034a84f756794ea
Jelin <6531501042@lamduan.mfu.ac.th>
2025-10-17 17:16:07 +0700

    Create SidebarItemSecretary.vue

---
+++ FE/src/components/secretary/SidebarItemSecretary.vue
@@ -0,0 +1,35 @@
+<!-- 📁 src/components/secretary/SidebarItemSecretary.vue -->
+<template>
+  <div class="flex items-center space-x-3 p-2 cursor-pointer hover:bg-blue-800 rounded">
+    <component :is="resolveIcon(icon)" class="w-5 h-5" />
+    <span @click="handleClick">{{ label }}</span>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const props = defineProps({
+  icon: String,
+  label: String,
+  to: String
+})
+
+const router = useRouter()
+
+const resolveIcon = (iconName) => {
+  // ใช้ไอคอนพื้นฐานแทน หรือ import ไอคอนจริง
+  return 'span' // หรือใช้ไอคอนจริงจากไลบรารี
+}
+
+const handleClick = () => {
+  if (props.to) {
+    router.push(props.to)
+  } else if (props.label === 'Log out') {
+    // ปล่อยให้ parent จัดการ
+    emit('click')
+  }
+}
+
+const emit = defineEmits(['click'])
+</script>
